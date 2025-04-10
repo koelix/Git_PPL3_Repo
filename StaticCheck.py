6555d633f96d876455c96eff8d1e3ebdd0c08837
koelix <duy.nguyen210522@hcmut.edu.vn>
2025-04-11 04:22:09 +0700

    Bản chạy được chính thức đầu tiên

--- StaticCheck.py
+++ StaticCheck.py
@@ -47,27 +47,37 @@ class StaticChecker(BaseVisitor,Utils):
 
             FuncDecl("putLn", [], VoidType(), Block([]))
         ]
+        # function_current chỉ được dùng để check trong return
         self.function_current: FuncDecl = None
 
     def check(self):
         self.visit(self.ast, None)
 
     def checkType(self, LSH_type: Type, RHS_type: Type, list_type_permission: List[Tuple[Type, Type]] = []) -> bool:
+        # kiểm tra phía trái là struct và phía bên phải là nil (nil được trả về struct type với name là rỗng)
         if type(RHS_type) == StructType and RHS_type.name == "":
             return # TODO: Implement
 
+        # chuyển kiểu id về struct/interface
         LSH_type = self.lookup(LSH_type.name, self.list_type, lambda x: x.name) if isinstance(LSH_type, Id) else LSH_type
         RHS_type = self.lookup(RHS_type.name, self.list_type, lambda x: x.name) if isinstance(RHS_type, Id) else RHS_type
+
+        # kiểm tra các cặp cho phép có thể float = int, inteface = struct
         if (type(LSH_type), type(RHS_type)) in list_type_permission:
             if isinstance(LSH_type, InterfaceType) and isinstance(RHS_type, StructType):
+                # nếu mà inteface = struct cần phải kiểm tra các hàm trong inface có trong struct hay không (bao gồm return, params, name)
                 return # TODO: Implement
             return True
 
-        if # TODO: Implement:
-            return LSH_type.name == RHS_type.name
+        # so sánh struct/interface
+        # if # TODO: Implement:
+        #     return LSH_type.name == RHS_type.name
 
+        # so sánh array
         if isinstance(LSH_type, ArrayType) and isinstance(RHS_type, ArrayType):
             return  # TOOO với các phần tử trong dimens đều là IntLiteral (này sẽ được chuyển đổi ở bước sau)
+
+        # so sánh kiểu bình thường
         return type(LSH_type) == type(RHS_type)
 
     def visitProgram(self, ast: Program,c : None):
@@ -124,10 +134,13 @@ class StaticChecker(BaseVisitor,Utils):
         )
 
     def visitStructType(self, ast: StructType, c : List[Union[StructType, InterfaceType]]) -> StructType:
-        res = # TODO: Implement
+        # kiểm tra xem đã có Type nào trùng tên hay chưa để nén ra lỗi Redeclared
+        res = None # TODO: Implement
 
         def visitElements(element: Tuple[str,Type], c: List[Tuple[str,Type]]) -> Tuple[str,Type]:
+            pass
             # TODO: Implement
+            # dùng để đệ quy cho elements để tìm lỗi Redeclared
 
         ast.elements = reduce(lambda acc,ele: [visitElements(ele,acc)] + acc , ast.elements , [])
         return ast
@@ -156,8 +169,16 @@ class StaticChecker(BaseVisitor,Utils):
         res = self.lookup(ast.name, c[0], lambda x: x.name)
         if not res is None:
             raise Redeclared(Function(), ast.name)
+
+        # Gán function đang chạy
+        self.function_current = ast
+
         # visit block và lấy ra danh sách param và chuyển thành Symbol trong tầm vực mới
         self.visit(ast.body, [list(reduce(lambda acc,ele: [self.visit(ele,acc)] + acc, ast.params, []))] + c)
+
+        # Hủy gán function đang chạy
+        self.function_current = None
+
         # trả về Symbol tương ứng với Type là FuntionType
         return Symbol(ast.name, ast.retType, None)
         # TODO: Implement - Done Trả về Symbol của Symbol()
@@ -336,10 +357,10 @@ class StaticChecker(BaseVisitor,Utils):
     def evaluate_ast(self, node: AST, c: List[List[Symbol]]) -> int:
         if type(node) == IntLiteral:
             return int(node.value)
-        elif type(node) == Id:
-            res =  # TODO: TÌM GIÁ TRỊ
-            return
-            ## TODO binary và Unary, các trường hợp còn lại sẽ không hợp lệ vì sẽ không là kiểu int và thầy đã thông báo trên forum
+        # elif type(node) == Id:
+        #     res =  # TODO: TÌM GIÁ TRỊ
+        #     return
+        #     ## TODO binary và Unary, các trường hợp còn lại sẽ không hợp lệ vì sẽ không là kiểu int và thầy đã thông báo trên forum
         return 0
 
     def visitAssign(self, ast: Assign, c: List[List[Symbol]]) -> None:
@@ -368,10 +389,13 @@ class StaticChecker(BaseVisitor,Utils):
     def visitContinue(self, ast, c: List[List[Symbol]]) -> None: return None
     def visitBreak(self, ast, c: List[List[Symbol]]) -> None: return None
     def visitReturn(self, ast, c: List[List[Symbol]]) -> None:
-        if not self.checkType(# TODO: Implement, self.function_current.retType):
-            raise TypeMismatch(ast)
+        # - so sánh type trả về với function_current (nếu không trả về mặt định vào void)
+        # if not self.checkType(# TODO: Implement, self.function_current.retType):
+        #     raise TypeMismatch(ast)
         return None
+
     def visitBinaryOp(self, ast: BinaryOp, c: List[List[Symbol]]):
+        # - kiểm tra kiểu LHS và RHS trước nếu bằng nhau xét đến kiểu bên trong và type trả về
         LHS_type = self.visit(ast.left, c)
         RHS_type = self.visit(ast.right, c)
 
@@ -393,15 +417,18 @@ class StaticChecker(BaseVisitor,Utils):
         # TODO: Implement
 
     def visitArrayCell(self, ast: ArrayCell, c: List[List[Symbol]]):
-        array_type = # TODO: Implement
+        # visit đến phần tử expr
+        array_type = None # TODO: Implement
         if not isinstance(array_type, ArrayType):
             raise TypeMismatch(ast)
 
-        if not all(map(lambda item: self.checkType(self.visit(item, c), # TODO: Implement), ast.idx)):
-            raise TypeMismatch(ast)
+        # if not all(map(lambda item: self.checkType(self.visit(item, c), # TODO: Implement), ast.idx)):
+        #     raise TypeMismatch(ast)
         if len(array_type.dimens) == len(ast.idx):
+            # trả về type khi giảm chiều (chiều 0 là type bên trong phần tử)
             return # TODO: Implement
         elif len(array_type.dimens) > len(ast.idx):
+            # trả về tyep khi giảm chiều
             return # TODO: Implement
         raise TypeMismatch(ast)
 
@@ -415,7 +442,7 @@ class StaticChecker(BaseVisitor,Utils):
                 list(map(lambda value: nested2recursive(value, c), dat))
             else:
                 self.visit(dat, c)
-        nested2recursive(# TODO: Implement)
+        nested2recursive()# TODO: Implement)
         return ArrayType(ast.dimens, ast.eleType)
     def visitStructLiteral(self, ast:StructLiteral, c: List[List[Symbol]]) -> Type:
         list(map(lambda value: self.visit(value[1], c), ast.elements))
